a812cf8dc88de0cc2c5d4d32c919fa3aeaaca0e8
kraglr <klaguilar.1doc@gmail.com>
2022-09-03 14:31:38 +0800

    Update HomeController.php
    create new function geticdCode

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'verified']);
     }
 
     /**
@@ -24,5 +26,25 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    public function getICDCODE($icd10){
+
+        $getICDdesc = DB::table('u_hisicd10s')->where('icd10Code',$icd10)->first();
+
+        // dd($getICDdesc);
+        return response()->json([
+            'icdGet'=>$getICDdesc,
+            
+            // 'icd10codes'=>$post9,
+        ]);
+
+    }
+
+    public function check(){
+        if(Auth::user()){
+            return view('home');
+        }else{
+            return view('auth.login');
+        }
     }
 }
