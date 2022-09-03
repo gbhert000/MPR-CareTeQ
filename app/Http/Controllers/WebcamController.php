b64dc60354ebc6c165b018ef9b0315f89f448a32
kraglr <klaguilar.1doc@gmail.com>
2022-09-03 14:32:57 +0800

    Update WebcamController.php
    change the flow of saving image

--- app/Http/Controllers/WebcamController.php
+++ app/Http/Controllers/WebcamController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
     
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 use Storage;
   
 class WebcamController extends Controller
@@ -26,38 +27,52 @@ class WebcamController extends Controller
     public function store(Request $request)
     {
         $img = $request->image;
-        // dd($request->image);
-        // dd($img);
-        $folderPath = "uploads/";
-        
-        $image_parts = explode(";base64,", $img);
-        $image_type_aux = explode("image/", $image_parts[0]);
-        $image_type = $image_type_aux[1];
-        
-        $image_base64 = base64_decode($image_parts[1]);
-        $fileName = uniqid() . '.png';
-        
-        $file = $folderPath . $fileName;
+        // $img=str_replace('data:image/png;base64,', '', $img);
 
+
+        // for txt file
+        $img=str_replace(' ', '+', $img);
+        // $img_name=uniqid().'.txt';
+        // $folderPath = "uploads/";
+        // $file = $folderPath . $img_name;
+        // $uploadStorage=Storage::disk('my_files')->put($file,$img);
+
+        // dd($request->imagebase64);
+        // dd($img);
+               
+        // $image_parts = explode(";base64,", $img);
+        // // dd($image_parts[0]);
+        // $image_type_aux = explode("image/", $image_parts[0]);
+        // $image_type = $image_type_aux[1];
+        
+        // $image_base64 = base64_decode($image_parts[1]);
+        // $fileName = uniqid() . '.png';
+        // // dd($fileName);
+        
         // $uploadStorage=$img->store('toPath', ['disk' => 'my_files']);
         // dd($file);
-        $uploadStorage=Storage::disk('my_files')->put($file, $image_base64);
+        // $uploadStorage=Storage::disk('my_files')->put($file,$img);
+        // \File::put(storage_path(). '/' . $imageName, base64_decode($image));
 
-        $fullName=strtoupper(join('',[$request->lnameImage,',',$request->fnameImage, $request->mnameImage, $request->enameImage]));
+        // $fullName=strtoupper(join('',[$request->lnameImage.',',$request->fnameImage, $request->mnameImage, $request->enameImage]));
+        // dd($fullName);
 
-        $uploadDatabase=DB::table('u_hisimages')->updateOrInsert(
-            ['patientCode'=>$request->patientCode],
-                ['imageName'=>$fileName,'patientCode'=>$request->patientCode,'patientName'=>$fullName]
-        );
-          if($uploadStorage&&$uploadDatabase){
+        // $uploadDatabase=DB::table('u_hisimages')->updateOrInsert(
+        //     ['patientCode'=>$request->patientCode],
+        //         ['imageName'=>$img_name,'patientCode'=>$request->patientCode,'patientName'=>$fullName]
+        // );
+        // dd($img);
+
+        //   if($uploadStorage){
             // dd(($selectRecord1));
             return response()->json(['success' => true,
-                        'msg' => 'Uploaded Successfully',       
+                        'msg' => 'Uploaded Successfully', 
+                        'img'=>$img      
             ]);
-        }
-        else{
-            return response()->json(['success' => false, 'msg' => 'Patient Successfully Registered.']);
-        }
-        // return view('home');
+        // }
+        // else{
+        //     return response()->json(['success' => false, 'msg' => 'Patient Successfully Registered.']);
+        // }
+       
     }
 }
