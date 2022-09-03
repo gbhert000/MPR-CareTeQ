86e8464cebf8696f65803bd0bd679cfbf25b376d
kraglr <klaguilar.1doc@gmail.com>
2022-09-03 14:32:06 +0800

    Update UserController.php
    add validation of email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
+
+    public function validateuseremail(Request $request)
+   {
+    $user = User::where('email', $request->email)->first('email');
+       if($user){
+         $return =  false;
+
+        }
+        else{
+         $return= true;
+        }
+        echo json_encode($return);
+        exit;
+   }
 }
